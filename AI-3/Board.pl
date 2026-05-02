22ffec86227bd02c8eb1a68212d70a1bd4a68718
unknown <aliomarx82@gmail.com>
2026-05-02 17:38:47 +0300

    Board became a 2D list added

--- AI-3/Board.pl
+++ AI-3/Board.pl
@@ -1,30 +1,17 @@
 % 11x11 Board
-board([
-    % King
-    piece(king, 6, 6),
-    
-    % Defenders
-    piece(defender, 6, 4), piece(defender, 6, 5), piece(defender, 6, 7), piece(defender, 6, 8),
-    piece(defender, 4, 6), piece(defender, 5, 6), piece(defender, 7, 6), piece(defender, 8, 6),
-    piece(defender, 5, 5), piece(defender, 5, 7), piece(defender, 7, 5), piece(defender, 7, 7),
-
-    % Attackers
-    % Top
-    piece(attacker, 1, 4), piece(attacker, 1, 5), piece(attacker, 1, 6), 
-    piece(attacker, 1, 7), piece(attacker, 1, 8), piece(attacker, 2, 6),
-
-    % Bottom
-    piece(attacker, 11, 4), piece(attacker, 11, 5), piece(attacker, 11, 6), 
-    piece(attacker, 11, 7), piece(attacker, 11, 8), piece(attacker, 10, 6),
-
-    % Left
-    piece(attacker, 4, 1), piece(attacker, 5, 1), piece(attacker, 6, 1), 
-    piece(attacker, 7, 1), piece(attacker, 8, 1), piece(attacker, 6, 2),
-
-    % Right
-    piece(attacker, 4, 11), piece(attacker, 5, 11), piece(attacker, 6, 11), 
-    piece(attacker, 7, 11), piece(attacker, 8, 11), piece(attacker, 6, 10)
-]).
+board([ 
+    [c, e, e, a, a, a, a, a, e, e, c], 
+    [e, e, e, e, e, a, e, e, e, e, e], 
+    [e, e, e, e, e, e, e, e, e, e, e], 
+    [a, e, e, e, e, d, e, e, e, e, a], 
+    [a, e, e, e, d, d, d, e, e, e, a], 
+    [a, a, e, d, d, k, d, d, e, a, a], 
+    [a, e, e, e, d, d, d, e, e, e, a], 
+    [a, e, e, e, e, d, e, e, e, e, a], 
+    [e, e, e, e, e, e, e, e, e, e, e], 
+    [e, e, e, e, e, a, e, e, e, e, e], 
+    [c, e, e, a, a, a, a, a, e, e, c] 
+]). 
 
 % State Representaion
 initial_state(state(Board, attacker)) :- 
