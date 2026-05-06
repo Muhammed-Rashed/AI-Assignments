abda3195e76a02d141a9335e25e6f0e4d431de5f
unknown <aliomarx82@gmail.com>
2026-05-06 14:48:28 +0300

    changed to 9x9

--- AI-3/Board.pl
+++ AI-3/Board.pl
@@ -1,17 +1,15 @@
-% 11x11 Board
-board([ 
-    [e, e, e, a, a, a, a, a, e, e, e], 
-    [e, e, e, e, e, a, e, e, e, e, e], 
-    [e, e, e, e, e, e, e, e, e, e, e], 
-    [a, e, e, e, e, d, e, e, e, e, a], 
-    [a, e, e, e, d, d, d, e, e, e, a], 
-    [a, a, e, d, d, k, d, d, e, a, a], 
-    [a, e, e, e, d, d, d, e, e, e, a], 
-    [a, e, e, e, e, d, e, e, e, e, a], 
-    [e, e, e, e, e, e, e, e, e, e, e], 
-    [e, e, e, e, e, a, e, e, e, e, e], 
-    [e, e, e, a, a, a, a, a, e, e, e] 
-]). 
+% 9x9 Board
+board([
+    [e, e, e, a, a, a, e, e, e],
+    [e, e, e, e, a, e, e, e, e],
+    [e, e, e, e, d, e, e, e, e],
+    [a, e, e, e, d, e, e, e, a],
+    [a, a, d, d, k, d, d, a, a],
+    [a, e, e, e, d, e, e, e, a],
+    [e, e, e, e, d, e, e, e, e],
+    [e, e, e, e, a, e, e, e, e],
+    [e, e, e, a, a, a, e, e, e]  
+]).
 
 % State Representaion
 initial_state(state(Board, attacker)):- 
@@ -20,18 +18,18 @@ initial_state(state(Board, attacker)):-
 % Define board borders
 edge(0,_).
 edge(_,0).
-edge(10,_).
-edge(_,10).
+edge(8,_).
+edge(_,8).
 
 % Define board corners
 corner(0,0).
-corner(0,10).
-corner(10,0).
-corner(10,10).
+corner(0,8).
+corner(8,0).
+corner(8,8).
 
 % Define special cells
 special_cell(R,C) :- corner(R,C). % Corners are special
-special_cell(5,5). % The throne
+special_cell(4,4). % The throne
 
 
 % Get the piece in a specific position
